398746f5ff670bc7320b25d2ea113102a4c5051d
fbin <fbinemail@163.com>
2018-03-31 09:54:00 +0800

    v0.0.1

---
+++ app/src/main/java/com/qs/tm/MainActivity.java
@@ -0,0 +1,32 @@
+package com.qs.tm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 主画面
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private Button loginButton = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new LoginButtonClickListener());
+    }
+
+    private class LoginButtonClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this,R.string.text_value,Toast.LENGTH_SHORT).show();
+        }
+    }
+}
